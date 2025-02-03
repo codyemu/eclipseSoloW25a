5c5b35a1f4f103d4a2a7dc41b845d0771dc527a6
codyemu <cmusulin@emich.edu>
2025-02-03 12:18:52 -0500

    programmer2 added to println

--- src/eclipseProjA/HelloWorld.java
+++ src/eclipseProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("This is another print");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this ProgrammerOne did this");
 
 	}
 
